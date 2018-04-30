645f11a58e1a388f43bc6a3c412118959fbff304
likly <569553290@qq.com>
2018-04-30 17:58:48 +0800

    edit randomIntGenerator test

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,31 @@
 package tw.core;
 
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+
+    private RandomIntGenerator randomIntGenerator;
+
+    @Before
+    public void setup(){
+        this.randomIntGenerator = new RandomIntGenerator();
+    }
+
+    @Test
+    public void test_right(){
+        System.out.println(randomIntGenerator.generateNums(4,4));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_exception(){
+        randomIntGenerator.generateNums(1,4);
+    }
 
 }
